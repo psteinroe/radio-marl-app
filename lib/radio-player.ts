85a19c7e1d3e877815d5bb6e46adf39127e07b87
psteinroe <philipp@steinroetter.com>
2026-08-01 08:19:02 +0000

    fix(audio): disable ICY metadata interleaving

--- lib/radio-player.ts
+++ lib/radio-player.ts
@@ -3,7 +3,10 @@ import { createLivePlayerController } from "./live-player-controller";
 
 export const RADIO_TRACK: MediaItem = {
 	mediaId: "radio-marl-live",
-	url: "https://c32.radioboss.fm:18152/stream",
+	url: {
+		uri: "https://c32.radioboss.fm:18152/stream",
+		headers: { "Icy-MetaData": "0" },
+	},
 	title: "Radio Marl",
 	artist: "Live",
 	artworkUrl: require("../assets/images/icon.png"),
